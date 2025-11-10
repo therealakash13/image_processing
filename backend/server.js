f78bd28598f6a0385d89c91d077a18d1dffc5207
Akash <therealakash13@gmail.com>
2025-11-10 12:30:31 +0530

    connected frontend and backend using axios and cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const server = express();
 const port = 3000;
+server.use(express.json());
+server.use(cors({ origin: "http://localhost:5173" }));
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.get("/", (req, res) => {
   res.send("Hello... from image processor...");
+});
+server.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Received...");
 });
 
 server.listen(port, () => console.log(`Server is running on port ${port}`));
--- frontend/src/component/Form.jsx
+++ frontend/src/component/Form.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Form() {
+  const [name, setName] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+    try {
+      const response = await axios.post("http://localhost:3000/", {name});
+      console.log(response);
+      setName("");
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <>
-      <h1>This is form component</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+        {/* <input type="text" name="" id="" /> */}
+      </form>
     </>
   );
 }
